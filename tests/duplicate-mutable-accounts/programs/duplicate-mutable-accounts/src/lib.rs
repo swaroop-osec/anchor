a6f6c572de5adaca778fd8ec1889c4d57f2bc69c
swaroop-osec <swaroop@osec.io>
2025-08-28 21:14:12 +0530

    fix: update program IDs

--- tests/duplicate-mutable-accounts/programs/duplicate-mutable-accounts/src/lib.rs
+++ tests/duplicate-mutable-accounts/programs/duplicate-mutable-accounts/src/lib.rs
@@ -1,7 +1,7 @@
 use anchor_lang::prelude::*;
 
 // Intentionally different program id than the one defined in Anchor.toml.
-declare_id!("4D6rvpR7TSPwmFottLGa5gpzMcJ76kN8bimQHV9rogjH");
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
 pub mod duplicate_mutable_accounts {
