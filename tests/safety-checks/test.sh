84d1d6db8467a20cd92faeab4a126e8a16cec9b4
swaroop-osec <swaroop@osec.io>
2025-10-23 17:07:14 +0530

    chore(tests): Fix safety-checks test

--- tests/safety-checks/test.sh
+++ tests/safety-checks/test.sh
@@ -2,36 +2,40 @@
 
 echo "Building programs"
 
-#
-# Build the UncheckedAccount variant.
-#
-pushd programs/unchecked-account/
+mkdir -p tmp
+
+# Test unchecked-account
+mv programs/account-info tmp/
+mv programs/ignore-non-accounts tmp/
 output=$(anchor keys sync && anchor build 2>&1 > /dev/null)
 if ! [[ $output =~ "Struct field \"unchecked\" is unsafe" ]]; then
-   echo "Error: expected /// CHECK error"
+   echo "Error: expected /// CHECK error in programs/unchecked-account"
    exit 1
 fi
-popd
+mv tmp/account-info programs/
+mv tmp/ignore-non-accounts programs/
 
-#
-# Build the AccountInfo variant.
-#
-pushd programs/account-info/
+# Test account-info
+mv programs/unchecked-account tmp/
+mv programs/ignore-non-accounts tmp/
 output=$(anchor keys sync && anchor build 2>&1 > /dev/null)
 if ! [[ $output =~ "Struct field \"unchecked\" is unsafe" ]]; then
-   echo "Error: expected /// CHECK error"
+   echo "Error: expected /// CHECK error in programs/account-info"
    exit 1
 fi
-popd
+mv tmp/unchecked-account programs/
+mv tmp/ignore-non-accounts programs/
 
-#
-# Build the control variant.
-#
-pushd programs/ignore-non-accounts/
+# Test ignore-non-accounts
+mv programs/unchecked-account tmp/
+mv programs/account-info tmp/
 if ! anchor keys sync && anchor build ; then
-   echo "Error: anchor build failed when it shouldn't have"
+   echo "Error: anchor build failed when it shouldn't have in programs/ignore-non-accounts"
    exit 1
 fi
-popd
+mv tmp/unchecked-account programs/
+mv tmp/account-info programs/
+
+rmdir tmp
 
 echo "Success. As expected, all builds failed that were supposed to fail."
