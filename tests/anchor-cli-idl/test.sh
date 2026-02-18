b8fdc9beea5bc09610c20e305e09c80c9ddd34e0
swaroop-osec <swaroop@osec.io>
2026-02-18 16:37:03 +0530

    chore(tests): Setup and use program_metadata for IDL CLI tests

--- tests/anchor-cli-idl/test.sh
+++ tests/anchor-cli-idl/test.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+DEPLOYER_KEYPAIR="keypairs/deployer-keypair.json"
+PROGRAM_ONE="2uA3amp95zsEHUpo8qnLMhcFAUsiKVEcKHXS1JetFjU5"
+
 # Write a keypair for program deploy
 mkdir -p target/deploy
 cp keypairs/idl_commands_one-keypair.json target/deploy
@@ -28,22 +31,35 @@ echo '{
   ]
 }' > testLargeIdl.json
 
-
+# Dump the Program Metadata Program from mainnet for local testing
+PMP_SO="target/deploy/program_metadata.so"
+if [ ! -f "$PMP_SO" ]; then
+  echo "Dumping Program Metadata Program from mainnet"
+  solana program dump ProgM6JCCvbYkfKqJYHePx4xxSUSqJp7rh8Lyv7nk7S "$PMP_SO" --url https://api.mainnet-beta.solana.com
+fi
 
 echo "Building programs"
 
-anchor build
+anchor build --ignore-keys
 
 echo "Starting local validator for test"
 
 solana-test-validator --reset \
   -q \
   --mint tgyXxAhCkpgtKCEi4W6xWJSzqwVGs3uk2RodbZP2J49 \
-  --bpf-program 2uA3amp95zsEHUpo8qnLMhcFAUsiKVEcKHXS1JetFjU5 target/deploy/idl_commands_one.so \
-  --bpf-program DE4UbHnAcT6Kfh1fVTPRPwpiA3vipmQ4xR3gcLwX3wwS target/deploy/idl_commands_one.so \
+  --bpf-program ProgM6JCCvbYkfKqJYHePx4xxSUSqJp7rh8Lyv7nk7S "$PMP_SO" \
+  --upgradeable-program 2uA3amp95zsEHUpo8qnLMhcFAUsiKVEcKHXS1JetFjU5 target/deploy/idl_commands_one.so tgyXxAhCkpgtKCEi4W6xWJSzqwVGs3uk2RodbZP2J49 \
+  --upgradeable-program DE4UbHnAcT6Kfh1fVTPRPwpiA3vipmQ4xR3gcLwX3wwS target/deploy/idl_commands_one.so tgyXxAhCkpgtKCEi4W6xWJSzqwVGs3uk2RodbZP2J49 \
   &
 
 sleep 10
+
+echo "Deploying IDL via program-metadata"
+
+# Note: Since anchor idl init/upgrade skip localnet, we need to deploy the IDL via program-metadata.
+# Deploy IDL for program one 
+program-metadata --keypair "$DEPLOYER_KEYPAIR" --rpc http://localhost:8899 \
+  write idl "$PROGRAM_ONE" target/idl/idl_commands_one.json
 
 echo "Running tests"
 
