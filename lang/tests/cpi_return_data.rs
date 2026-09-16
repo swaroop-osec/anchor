a7b5cb58ad43c192504334dfd1bac4b6031b29b2
Jamie Hill-Daniel <134328753+jamie-osec@users.noreply.github.com>
2026-09-16 16:15:40 +0100

    fix(lang): Store CPI return data in Vec (#4931)

--- lang/tests/cpi_return_data.rs
+++ lang/tests/cpi_return_data.rs
@@ -5,17 +5,17 @@ fn test_cpi_return_data_snapshot_keeps_original_bytes() {
     let program_id = Pubkey::new_unique();
     let original = 10u64;
     let original_bytes = borsh::to_vec(&original).unwrap();
-    let snapshot = CpiReturnData::new(Some((program_id, original_bytes.clone())));
+    let snapshot = CpiReturnData::new(program_id, original_bytes.clone());
 
     let spoofed = 999u64;
     let spoofed_bytes = borsh::to_vec(&spoofed).unwrap();
-    let later = CpiReturnData::new(Some((program_id, spoofed_bytes.clone())));
+    let later = CpiReturnData::new(program_id, spoofed_bytes.clone());
 
-    let (snapshot_program_id, snapshot_bytes) = snapshot.return_data().unwrap();
+    let (snapshot_program_id, snapshot_bytes) = snapshot.return_data();
     assert_eq!(snapshot_program_id, program_id);
     assert_eq!(snapshot_bytes, original_bytes.as_slice());
 
-    let (later_program_id, later_bytes) = later.return_data().unwrap();
+    let (later_program_id, later_bytes) = later.return_data();
     assert_eq!(later_program_id, program_id);
     assert_eq!(later_bytes, spoofed_bytes.as_slice());
 
@@ -29,7 +29,7 @@ fn test_cpi_return_data_snapshot_rejects_program_id_mismatch() {
     let program_id = Pubkey::new_unique();
     let other_program_id = Pubkey::new_unique();
     let value = 10u64;
-    let snapshot = CpiReturnData::new(Some((other_program_id, borsh::to_vec(&value).unwrap())));
+    let snapshot = CpiReturnData::new(other_program_id, borsh::to_vec(&value).unwrap());
 
     let _ = snapshot.get::<u64>(program_id);
 }
