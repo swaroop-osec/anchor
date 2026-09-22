cc9f6b1c5e4a646bc592486a7bd485a8357a028d
Swaroop Maddu <swaroop@osec.io>
2026-09-22 20:33:55 +0530

    feat: add missing documentation and updates (#5053)

--- docs-v2/src/content/docs/updates/_meta.ts
+++ docs-v2/src/content/docs/updates/_meta.ts
@@ -6,5 +6,6 @@ export default {
     'release-notes': { order: 0 },
     changelog: { order: 1 },
     'contribution-guide': { order: 2 },
+    'backport-workflow': { label: 'Backport workflow', order: 3 },
   },
 } satisfies MetaFile
